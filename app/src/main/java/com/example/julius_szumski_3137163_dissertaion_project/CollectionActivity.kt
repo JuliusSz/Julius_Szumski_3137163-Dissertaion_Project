693962fa97e9091257c1a29ea2cae653ffc6ee14
JuliusSz <Julius.szumski@gmail.com>
2026-01-11 02:04:00 +0000

    Local BLE backend fixed up

--- app/src/main/java/com/example/julius_szumski_3137163_dissertaion_project/CollectionActivity.kt
+++ app/src/main/java/com/example/julius_szumski_3137163_dissertaion_project/CollectionActivity.kt
@@ -1,5 +1,6 @@
 package com.example.julius_szumski_3137163_dissertaion_project
 
+import android.content.ClipData
 import android.content.Context
 import android.content.Intent
 import android.hardware.Sensor
@@ -7,32 +8,44 @@ import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AddCircle
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Shapes
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import com.example.julius_szumski_3137163_dissertaion_project.ui.theme.Julius_Szumski_3137163Dissertaion_ProjectTheme
 import com.google.firebase.Firebase
 import com.google.firebase.firestore.DocumentSnapshot
@@ -102,7 +115,7 @@ class CollectionActivity : ComponentActivity() {
                     }
 
                 ) { innerPadding ->
-                    critterList(this,innerPadding)
+                    CritterList(this,innerPadding)
                 }
             }
         }
@@ -111,19 +124,21 @@ class CollectionActivity : ComponentActivity() {
     override fun onResume() {
         super.onResume()
         userID.value = intent.getStringExtra("userToken").toString()
+        //Toast.makeText(this,userID.value , Toast.LENGTH_SHORT).show()
         val db = Firebase.firestore
         db.collection("critter").whereEqualTo("User",userID.value).get().addOnSuccessListener { docs->
-            for (document in docs.documents){
-                critterCollection.add(document)
-            }
+            critterCollection.clear()
+            critterCollection.addAll(docs)
+            critterCount.value = critterCollection.size
         }
-
+        Toast.makeText(this,critterCollection.size.toString(), Toast.LENGTH_SHORT).show()
     }
     private  val userID = mutableStateOf<String>("")
     private var critterCollection= mutableListOf<DocumentSnapshot>()
+    private val critterCount = mutableStateOf<Int>(0)
 
     @Composable
-    fun critterList(context: Context, padding: PaddingValues){
+    fun CritterList(context: Context, padding: PaddingValues){
         LazyColumn(verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier
@@ -131,21 +146,51 @@ class CollectionActivity : ComponentActivity() {
                 .fillMaxWidth()
         ){
 
-            if (critterCollection.isEmpty()){
+            if (critterCount.value == 0){
                 item {
                     Text(
                         text = "You have found no Critters yet"
                     )
                 }
             }else{
-               for (critter in critterCollection){
+               val critterChunks = critterCollection.chunked(3)
+                for (chunk in critterChunks){
+                    item {
+                        Row(modifier = Modifier.fillMaxWidth()) {
+                            for (critter in chunk){
+                                when(critter.get("Type")){
+                                    "CommonPH" ->
+                                        Box(modifier = Modifier.weight(1f).padding(5.dp).height(100.dp).width(100.dp).border(3.dp, Color.Blue, RoundedCornerShape(10)), contentAlignment = Alignment.Center){
+                                            Card() {
+                                                Text(critter.get("Name").toString())
+                                            }
+                                        }
+                                    "RarePH" ->
+                                        Box(modifier = Modifier.weight(1f).padding(5.dp).height(100.dp).width(100.dp).border(3.dp, Color.Green, RoundedCornerShape(10)), contentAlignment = Alignment.Center){
+                                            Card() {
+                                                Text(critter.get("Name").toString())
+                                            }
+                                        }
+                                    "LegendaryPH" ->
+                                        Box(modifier = Modifier.weight(1f).padding(5.dp).height(100.dp).width(100.dp).border(3.dp, Color.Yellow, RoundedCornerShape(10)), contentAlignment = Alignment.Center){
+                                            Card() {
+                                                Text(critter.get("Name").toString())
+                                            }
+                                        }
+                                }
 
+                            }
+                            if (chunk.size< 3){
+                                val blankspaces: Int = 3-chunk.size
+                                repeat(blankspaces){
+                                    Spacer(modifier = Modifier.weight(1f))
+                                }
+                            }
 
-               }
-
+                        }
+                    }
+                }
             }
-
-
         }
     }
 }
--- app/src/main/java/com/example/julius_szumski_3137163_dissertaion_project/PlayerSearchActivity.kt
+++ app/src/main/java/com/example/julius_szumski_3137163_dissertaion_project/PlayerSearchActivity.kt
@@ -1,8 +1,32 @@
 package com.example.julius_szumski_3137163_dissertaion_project
 
+import android.Manifest
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothGatt
+import android.bluetooth.BluetoothGattCallback
+import android.bluetooth.BluetoothGattCharacteristic
+import android.bluetooth.BluetoothGattServer
+import android.bluetooth.BluetoothGattServerCallback
+import android.bluetooth.BluetoothGattService
+import android.bluetooth.BluetoothManager
+import android.bluetooth.BluetoothProfile
+import android.bluetooth.le.AdvertiseCallback
+import android.bluetooth.le.AdvertiseData
+import android.bluetooth.le.AdvertiseSettings
+import android.bluetooth.le.BluetoothLeAdvertiser
+import android.bluetooth.le.BluetoothLeScanner
+import android.bluetooth.le.ScanCallback
+import android.bluetooth.le.ScanFilter
+import android.bluetooth.le.ScanResult
+import android.bluetooth.le.ScanSettings
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.os.ParcelUuid
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -33,6 +57,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import com.example.julius_szumski_3137163_dissertaion_project.ui.theme.Julius_Szumski_3137163Dissertaion_ProjectTheme
+import java.util.UUID
 
 @ExperimentalMaterial3Api
 class PlayerSearchActivity : ComponentActivity() {
@@ -40,6 +65,29 @@ class PlayerSearchActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                val missingPermissions = mutableListOf<String>()
+                if (checkSelfPermission(Manifest.permission.BLUETOOTH_ADVERTISE) != PackageManager.PERMISSION_GRANTED)
+                    missingPermissions.add(Manifest.permission.BLUETOOTH_ADVERTISE)
+                if (checkSelfPermission(Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED)
+                    missingPermissions.add(Manifest.permission.BLUETOOTH_CONNECT)
+                if (checkSelfPermission(Manifest.permission.BLUETOOTH_SCAN) != PackageManager.PERMISSION_GRANTED)
+                    missingPermissions.add(Manifest.permission.BLUETOOTH_SCAN)
+                if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+                    missingPermissions.add(Manifest.permission.ACCESS_FINE_LOCATION)
+
+
+                if (missingPermissions.isNotEmpty()) {
+                    requestPermissions(missingPermissions.toTypedArray(), 2)
+                } else {
+                    initBLE()
+                }
+            } else {
+                initBLE()
+            }
+
+
             Julius_Szumski_3137163Dissertaion_ProjectTheme {
                 Scaffold(modifier = Modifier.fillMaxSize(),
                     topBar = {
@@ -56,6 +104,16 @@ class PlayerSearchActivity : ComponentActivity() {
                         }else{
                             searching.value=true
                             searchButtonColor.value = Color.Cyan
+
+                            BLEadvert.startAdvertising(BLEadvSettings,ComData,object : AdvertiseCallback(){})
+                            BLEscanner.startScan(listOf(scanFilter),BLEscanSettings,object : ScanCallback(){
+                                override fun onScanResult(callbackType: Int, result: ScanResult) {
+                                    BLEscanner.stopScan(this)
+                                    result.device.connectGatt(this@PlayerSearchActivity, false, gattCallback
+                                    )
+                                }
+                            })
+
                         }
 
                     }, containerColor = searchButtonColor.value){
@@ -148,6 +206,7 @@ class PlayerSearchActivity : ComponentActivity() {
                 .fillMaxWidth()
         ){
             if(searching.value){
+
                 if (foundPlayer.size == 0 ){
                     item {
                         Text(
@@ -167,4 +226,134 @@ class PlayerSearchActivity : ComponentActivity() {
 
         }
     }
+
+
+    private fun initBLE() {
+        blManager =  getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+        blAdapter= blManager.adapter
+        BLEadvert = blAdapter.bluetoothLeAdvertiser
+        BLEscanner= blAdapter.bluetoothLeScanner
+
+        gatt = blManager.openGattServer(this, object:BluetoothGattServerCallback(){
+            override fun onCharacteristicWriteRequest(
+                device: BluetoothDevice,
+                requestId: Int,
+                characteristic: BluetoothGattCharacteristic,
+                preparedWrite: Boolean,
+                responseNeeded: Boolean,
+                offset: Int,
+                value: ByteArray
+            ) {
+                val msg = String(value)
+
+                if (msg == "HELLO") {
+                    characteristic.value = "ACK".toByteArray()
+                }
+
+                gatt.sendResponse(
+                    device,
+                    requestId,
+                    BluetoothGatt.GATT_SUCCESS,
+                    0,
+                    characteristic.value
+                )
+            }
+            override fun onCharacteristicReadRequest(
+                device: BluetoothDevice,
+                requestId: Int,
+                offset: Int,
+                characteristic: BluetoothGattCharacteristic
+            ) {
+                gatt.sendResponse(
+                    device,
+                    requestId,
+                    BluetoothGatt.GATT_SUCCESS,
+                    offset,
+                    characteristic.value
+                )
+            }
+        })
+        gatt.addService(service)
+        gattCallback = object : BluetoothGattCallback(){
+
+            override fun onConnectionStateChange(gatt: BluetoothGatt?, status: Int, newState: Int) {
+                if (newState == BluetoothProfile.STATE_CONNECTED) {
+                    gatt!!.discoverServices()
+                }
+            }
+
+            override fun onServicesDiscovered(gatt: BluetoothGatt?, status: Int){
+                val characteristic = gatt!!.getService(BLEIdentifier).getCharacteristic(HandshakeIdentifier)
+                characteristic.value = "HELLO".toByteArray()
+                gatt.writeCharacteristic(characteristic)
+            }
+
+            override fun onCharacteristicRead(
+                gatt: BluetoothGatt,
+                characteristic: BluetoothGattCharacteristic,
+                status: Int
+            ) {
+                val response = String(characteristic.value)
+                if(response == "ACK"){
+                    runOnUiThread {
+                        Toast.makeText(this@PlayerSearchActivity,"Conection Established",Toast.LENGTH_SHORT).show()
+
+                    }
+
+                }
+            }
+
+
+
+
+            override fun onCharacteristicWrite(
+                gatt: BluetoothGatt,
+                characteristic: BluetoothGattCharacteristic,
+                status: Int
+            ) {
+                gatt.readCharacteristic(characteristic)
+            }
+
+        }
+        Toast.makeText(this@PlayerSearchActivity,"successfully initialized", Toast.LENGTH_SHORT).show()
+    }
+    //GattServer manages Bluetooth Conection (reciever will act as server)
+    //https://www.uuidgenerator.net
+    val BLEIdentifier: UUID = UUID.fromString("ea86a980-7185-4e52-a37a-11e915c015c8") // Identifies The BLE "frequency" to other devices running the app
+    val HandshakeIdentifier: UUID = UUID.fromString("12a0b2fa-d7c0-4090-9287-a8a1903fb410") // Identifies The exchanged PlayerID as the Data that the other device scans for
+    lateinit var blManager: BluetoothManager
+    lateinit var blAdapter : BluetoothAdapter
+
+    val handshakeID = BluetoothGattCharacteristic(HandshakeIdentifier,BluetoothGattCharacteristic.PROPERTY_READ or BluetoothGattCharacteristic.PROPERTY_WRITE, BluetoothGattCharacteristic.PERMISSION_READ or BluetoothGattCharacteristic.PERMISSION_WRITE)
+    val service = BluetoothGattService(BLEIdentifier, BluetoothGattService.SERVICE_TYPE_PRIMARY).apply {
+        addCharacteristic(handshakeID)
+    }
+    lateinit var gatt: BluetoothGattServer
+
+    //BLE Advertiser
+    lateinit var BLEadvert : BluetoothLeAdvertiser
+    val BLEadvSettings = AdvertiseSettings.Builder().setAdvertiseMode(AdvertiseSettings.ADVERTISE_MODE_LOW_LATENCY).setConnectable(true).build()
+    val ComData = AdvertiseData.Builder().addServiceUuid(ParcelUuid(BLEIdentifier)).build()
+
+    //BLE Scanner
+
+    lateinit var BLEscanner: BluetoothLeScanner
+    val scanFilter = ScanFilter.Builder().setServiceUuid(ParcelUuid(BLEIdentifier)).build()
+    val BLEscanSettings = ScanSettings.Builder().build()
+
+    //handshake handling
+    lateinit var gattCallback: BluetoothGattCallback
+
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 2) {
+            if (grantResults.all { it == PackageManager.PERMISSION_GRANTED }) {
+                initBLE()
+            } else {
+                Toast.makeText(this, "BLE permissions are required", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
 }
